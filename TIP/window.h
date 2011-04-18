ba3f8947e608a827a64db5bfc36e2bbac5f2839e
mcdonaldrossc <mcdonaldrossc@gmail.com>
2011-04-18 21:42:03 +0000

    Changed the layout of the files

---
+++ TIP/window.h
@@ -0,0 +1,20 @@
+#ifndef WINDOW_H
+#define WINDOW_H
+
+#include <QWidget>
+
+/*
+ This is the main window class for TIP. It will hold all of the widgets and buttons
+ to operate the program correctly
+*/
+
+class Window : public QWidget
+{
+    Q_OBJECT
+
+public:
+    Window();
+
+};
+
+#endif // WINDOW_H
