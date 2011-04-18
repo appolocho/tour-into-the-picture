af8a291187b0daf3b9a333f505eb741bc0a1cdc7
mcdonaldrossc@gmail.com <mcdonaldrossc@gmail.com@ed69e75d-f0de-1164-67f9-19b70f95b7b5>
2011-04-18 22:25:25 +0000

    Fixed Layout

--- trunk/TIP/window.h
+++ trunk/TIP/window.h
@@ -7,6 +7,7 @@
  This is the main window class for TIP. It will hold all of the widgets and buttons
  to operate the program correctly
 */
+class QPushButton;
 
 class Window : public QWidget
 {
@@ -15,6 +16,13 @@ class Window : public QWidget
 public:
     Window();
 
+private:
+    QPushButton* quitButton; // button to quit program
+    QPushButton* loadBackButton; // button to load background image
+    QPushButton* saveButton; // button to save image
+    QPushButton* loadForeButton; // button to load foreground objects
+    QPushButton* placeVanButton; // button to place vanishing point
+    QPushButton* placeBackButton; // button to place background box
 };
 
 #endif // WINDOW_H
